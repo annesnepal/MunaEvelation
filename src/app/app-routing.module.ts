197f9f9b04fd962e5b0edb4e0e084eee8d357182
annesnepal <nepalanish98@gmail.com>
2023-07-30 16:15:13 +0545

    Login design fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { FeaturedComponent } from './layout/pages/featured/featured.component';
 import { ProductsComponent } from './layout/pages/products/products.component';
 import { ContactusComponent } from './layout/pages/contactus/contactus.component';
 import { LoginComponent } from './layout/pages/login/login.component';
+import { AdminDashboardComponent } from './layout/pages/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'contact-us', component: ContactusComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
 ];
 
 @NgModule({
--- src/app/layout/pages/login/login.module.ts
+++ src/app/layout/pages/login/login.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, LoginRoutingModule, MatIconModule],
 })
-export class LoginModule { }
+export class LoginModule {}
